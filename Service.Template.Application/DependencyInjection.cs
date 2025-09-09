875897bdee2c7c70e28a6e59d8ac5e2556fa47b8
Ahmad Awwad <100753799+aawwad172@users.noreply.github.com>
2025-09-09 11:44:55 +0300

    Initial commit

---
+++ Service.Template.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.Template.Application.Utilities;
+
+namespace Service.Template.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Register application services here
+        // Example: services.AddTransient<IMyService, MyService>();
+
+        MapsterConfigurations.RegisterMappings();
+
+        return services;
+    }
+}
---
+++ Service.Template.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.Template.Domain.Interfaces.Infrastructure;
+using Service.Template.Infrastructure.Persistence;
+
+namespace Service.Template.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        // Register infrastructure services here
+        // Example: services.AddTransient<IMyInfrastructureService, MyInfrastructureService>();
+
+        services.AddSingleton<IDbConnectionFactory, DbConnectionFactory>();
+
+        return services;
+    }
+}
